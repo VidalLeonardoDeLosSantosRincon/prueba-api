74041b54c1b59dd68fd9ccc840af3389bdf82ee1
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2019-09-14 13:31:47 -0400

    validating fields

--- js/js/login.js
+++ js/js/login.js
@@ -1,37 +1,59 @@
-let btnLog = document.getElementById("btn-login");
+(function(){
+    let btnLog = document.getElementById("btn-login");
+    let btnCancel = document.getElementById("btn-cancel");
 
-btnLog.addEventListener("click",toLogIn);
-let email;
-let password;
+    btnLog.addEventListener("click",toLogIn);
+    btnCancel.addEventListener("click",toCancel);
 
-function  toLogIn(){
-    let userToken;
-    email = document.forms["login-form"]["email"].value;
-    password = document.forms["login-form"]["password"].value;
-    console.log(`${email} ${password}`);
+    let email;
+    let password;
+    let loggedIn ;
+    loggedIn = false;
 
-    if(email.trim()!="" && password.trim()!=""){
-        fetch("https://apidev.kanvas.dev/v1/auth",{
-            method:"POST",
-            body:JSON.stringify({
-                email,
-                password
-            }),
-            header:{
-                "Content-Type":"application/json"
-            }
-        }).then((res)=>res.json())
-        .then((res)=>{
-            console.log(res);
-            console.log(res.token);
-            if(res.token){
-                userToken = res.token;
-                localStorage.setItem("userToken",userToken);
-            }
-        })
-        .catch((error)=>{console.log(`Error: ${error}`)});
-    }else{
-        alert("empty fields not allowed")
+    function toCancel(){
+        email = document.forms["login-form"]["email"].value;
+        password = document.forms["login-form"]["password"].value;
+        
+        if(email.trim()!=="" || password.trim()!==""){
+            document.forms["login-form"]["email"].value="";
+            document.forms["login-form"]["password"].value="";
+        }
     }
-    
-}
+
+    function  toLogIn(){
+        let userToken;
+        email = document.forms["login-form"]["email"].value;
+        password = document.forms["login-form"]["password"].value;
+        //console.log(`${email} ${password}`);
+
+        if(email.trim()!=="" && password.trim()!==""){
+            fetch("https://apidev.kanvas.dev/v1/auth",{
+                method:"POST",
+                body:JSON.stringify({
+                    email,
+                    password
+                }),
+                header:{
+                    "Content-Type":"application/json"
+                }
+            }).then((res)=>res.json())
+            .then((res)=>{
+                console.log(res);
+                console.log(res.token);
+                if(res.token){
+                    userToken = res.token;
+                    localStorage.setItem("userToken",userToken);
+                    loggedIn=true;
+                    if(loggedIn){
+                        localStorage.setItem("userIsLoggedIn",loggedIn);
+                        location.href="list.html";
+                    }
+                }
+            })
+            .catch((error)=>{console.log(`Error: ${error}`)});
+        }else{
+            alert("empty fields not allowed");
+        }
+        
+    }
+})();
